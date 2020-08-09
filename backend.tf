5aebf55a88dccbbaa4d8f638087eff53efa9c928
root <root@zahidali1c.mylabserver.com>
2020-08-09 16:39:57 +0000

    updated all files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "gcs" {
-   bucket = "my-tfstate-bucket"      # GCS bucket name to store terraform tfstate
-   prefix = "commerce-"
-   }
+  bucket = "my-tfstate-bucket"   # GCS bucket name to store terraform tfstate
+  prefix = "commerce"           # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
+  }
 }
