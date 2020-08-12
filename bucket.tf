afc9c28c9105c4aea1a7f3b51cc26c2ff80acf7a
zahid ali <zahid.ali.m@gmail.com>
2020-08-11 23:51:12 -0400

    added role

--- bucket.tf
+++ bucket.tf
@@ -5,6 +5,20 @@ project  = var.project_id
 region   = var.region
 }
 
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/storage.admin"
+    members = [
+      "user:cloud_user_p_91fb85f0@linuxacademygclabs.com",
+    ]
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket = google_storage_bucket.default.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
 # Create a GCS Bucket
 resource "google_storage_bucket" "my_bucket" {
 name     = var.bucket_name
